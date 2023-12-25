de78fc83928ac43fb98dca8dc1ca82b17506a775
moosa <moosaabdullahi45@gmail.com>
2023-12-25 23:39:38 +0200

    new

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,10 @@ import Footer from './ui/page/footer'
 
 export default function Home() {
   return (
-    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll 
+              scrollbar scrollbar-track-textDark/20
+              scrollbar-thumb-textDark/60
+              ">
       <Navbar />
       <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
         <motion.div 
